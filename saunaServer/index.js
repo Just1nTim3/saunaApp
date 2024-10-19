5ded80d17bb1c8973a760681a77b37ad359ea510
Konrad Dudek <kodu@netcompany.com>
2024-10-19 09:38:30 +0200

    added model and connected to mongo

--- saunaServer/index.js
+++ saunaServer/index.js
@@ -2,6 +2,17 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 const app = express()
+mongoose.connect('mongodb://localhost:27017/saunaTemps')
+
+const dataSchema = new mongoose.Schema({
+    temp: String,
+    timestamp: String
+},{
+    colletion: "temps"
+})
+
+const Model = mongoose.model('temps', dataSchema)
+
 
 app.listen(8080, () => {
     console.log("Server start")
@@ -11,5 +22,9 @@ app.get('/test', async (req, res) => {
     res.send("Test")
 })
 
+app.get('/all', async (req, res) => {
+    const data = await Model.find()
+    res.json(data)
+})
 
 
