0eb85b898b6c87c3282e3431f9f53f2077825682
Konrad Dudek <kodu@netcompany.com>
2024-10-22 23:34:13 +0200

    added sample get request to microcontroller

--- saunaTemp/saunaTemp.ino
+++ saunaTemp/saunaTemp.ino
@@ -1,19 +1,67 @@
+
+//Temps
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
 #define ONE_WIRE_BUS 4
+
+//Secrets
+
+#include <MySecrets.h>
+#define wifiName WIFI_NAME
+#define wifiPassword WIFI_PASSWORD
+
+//Web
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+String servicePath = "http://192.168.0.115:8080/test";
 
 void setup() {
   Serial.begin(9600);
   sensors.begin();
+
+  WiFi.begin(wifiName, wifiPassword);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
   sensors.requestTemperatures();
   Serial.print("Temp: ");
-  Serial.println(sensors.getTempCByIndex(0)); 
+  Serial.println(sensors.getTempCByIndex(0));
+
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    HTTPClient http;
+
+    http.begin(client, servicePath);
+    int responseCode = http.GET();
+    if (responseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(responseCode);
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(responseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
 
   delay(2000);
 }
